5a4b0d432ec29e42c2679fbab3124f4b15491d80
Da1-net12 <ivanspir07@mail.ru>
2021-09-01 20:03:47 +0300

    1236

---
+++ begin 11.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a:"))
+b = int(input("Enter a:"))
+sum = abs(a)+abs(b)
+raz = abs(a)-abs(b)
+chas = abs(a)/abs(b)
+proizv = abs(a)*abs(b)
+print("Вычисления равны: ",sum,raz,chas,proizv)
